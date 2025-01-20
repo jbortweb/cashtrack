71cf148493eb5a1e52d543d800d64ff61a0acb1e
Jbortweb <jbortweb@gmail.com>
2025-01-20 18:15:04 +0100

    conectar api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
 import { db } from './config/db'
+import budgetRouter from './routes/budgetRouter'
 
 export async function connectDB() {
   try {
@@ -19,5 +20,6 @@ const app = express()
 app.use(morgan('dev'))
 
 app.use(express.json())
+app.use('/api/budgets', budgetRouter)
 
 export default app
