6442a0400b5cbf0708318c246b7d6e252d58ab11
Jbortweb <jbortweb@gmail.com>
2025-02-04 20:08:42 +0100

    creado modelo usuarios

--- backend/src/models/Budget.ts
+++ backend/src/models/Budget.ts
@@ -1,15 +1,27 @@
-import { Column, DataType, Table, Model, HasMany } from 'sequelize-typescript'
+import {
+  Column,
+  DataType,
+  Table,
+  Model,
+  HasMany,
+  AllowNull,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript'
 import Expense from './Expense'
+import User from './User'
 
 @Table({
   tableName: 'budgets',
 })
 class Budget extends Model {
+  @AllowNull(false)
   @Column({
     type: DataType.STRING(100),
   })
   declare name: string
 
+  @AllowNull(false)
   @Column({
     type: DataType.DECIMAL,
   })
@@ -20,6 +32,12 @@ class Budget extends Model {
     onDelete: 'CASCADE',
   })
   declare expenses: Expense[]
+
+  @ForeignKey(() => User)
+  declare userId: number
+
+  @BelongsTo(() => User)
+  declare user: User
 }
 
 export default Budget
