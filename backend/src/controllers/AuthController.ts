4dd838de2e5b6bbeb5e0d4c7b603c55f92640cd2
Jbortweb <jbortweb@gmail.com>
2025-02-05 10:57:24 +0100

    install bcrypt y hasear password

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from 'express'
+import User from '../models/User'
+import { hashPassword } from '../utils/auth'
 
 export class AuthController {
-  static createAccount(req: Request, res: Response) {
-    res.json('createAccount funcionando')
+  static createAccount = async (req: Request, res: Response) => {
+    const { email, password } = req.body
+    const userExists = await User.findOne({ where: { email } })
+    if (userExists) {
+      const error = new Error('El email ya está en uso')
+      res.status(400).json({ error: error.message })
+      return
+    }
+    try {
+      const user = new User(req.body)
+      user.password = await hashPassword(password)
+      await user.save()
+      res.json('Cuenta creada correctamente')
+    } catch (error) {
+      res.status(500).json({ error: 'Hubo un error al crear la cuenta' })
+    }
   }
 }
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -38,11 +38,12 @@ class User extends Model {
   })
   declare token: string
 
-  @Default('false')
+  @Default(false)
+  @AllowNull(false)
   @Column({
     type: DataType.BOOLEAN,
   })
-  declare confirmed: string
+  declare confirmed: boolean
 
   @HasMany(() => Budget, {
     onUpdate: 'CASCADE',
