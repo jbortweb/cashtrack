e218d15a5646666c2aadd8afed87b9491365b8c9
Jbortweb <jbortweb@gmail.com>
2025-02-14 12:32:47 +0100

    state de token

--- frontend/components/auth/ConfirmAccountForm.tsx
+++ frontend/components/auth/ConfirmAccountForm.tsx
@@ -1,10 +1,25 @@
 'use client'
 import { PinInput, PinInputField } from '@chakra-ui/pin-input'
+import { useState } from 'react'
 
 const ConfirmAccountForm = () => {
+  const [token, setToken] = useState('')
+
+  const handleChange = (token: string) => {
+    setToken(token)
+  }
+
+  const handleComplete = (token: string) => {
+    console.log('Token:', token)
+  }
+
   return (
     <div className="flex gap-5 my-10 justify-center items-center">
-      <PinInput>
+      <PinInput
+        value={token}
+        onChange={handleChange}
+        onComplete={handleComplete}
+      >
         <PinInputField
           className="border border-gray-300 rounded-lg h-10 shadow-lg w-10 text-center  placeholder-white placeholder-white"
           type="text"
